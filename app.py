7f1171d8ce1a5616bdc240b31b581b79569bf1a0
Royce Lee - Li Ji <445081882@qq.com>
2023-10-10 12:31:21 +0800

    cache before create neww venv

--- app.py
+++ app.py
@@ -11,16 +11,16 @@ from langchain.schema.retriever import BaseRetriever
 from langchain.vectorstores.chroma import Chroma
 from langchain.vectorstores.faiss import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.chains import ConversationChain,ConversationalRetrievalChain
+from langchain.chains import ConversationChain, ConversationalRetrievalChain
 from langchain.chains.base import Chain
 from langchain.llms.openai import OpenAI
 from langchain.llms.base import BaseLLM
-from langchain.memory import ConversationKGMemory,ConversationBufferMemory
+from langchain.memory import ConversationKGMemory, ConversationBufferMemory
 from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
 
 
-def get_docs_from_pdfs()->List[Document]:
+def get_docs_from_pdfs() -> List[Document]:
     docs = []
     temp_dir = tempfile.TemporaryDirectory()
     for file in st.session_state["pdf_docs"]:
@@ -32,36 +32,40 @@ def get_docs_from_pdfs()->List[Document]:
     return docs
 
 
-def get_document_chunks(docs: List[Document])->List[Document]:
+def get_document_chunks(docs: List[Document]) -> List[Document]:
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     document_chunks = text_splitter.split_documents(docs)
     return document_chunks
 
 
-def get_vectorstore(document_chunks:List[Document])->VectorStore:
-    '''
+def get_vectorstore(document_chunks: List[Document]) -> VectorStore:
+    """
     This function first creates an embedding object,
     then returns a vectorstore from a list of documents.
-    '''
+    """
     embeddings = OpenAIEmbeddings(
         openai_api_key=os.getenv("OPENAI_API_KEY"),
     )
     # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     # vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-    vectorstore = Chroma.from_documents(documents=document_chunks,embedding=embeddings)
+    vectorstore = Chroma.from_documents(documents=document_chunks, embedding=embeddings)
     return vectorstore
 
 
-def get_retriever(vectorstore: VectorStore)->BaseRetriever:
+def get_retriever(vectorstore: VectorStore) -> BaseRetriever:
     retriever = vectorstore.as_retriever()
     return retriever
 
 
-def get_llm()->BaseLLM:
+def get_llm() -> BaseLLM:
     return OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
 
 
-def get_chain_by_pdfs()->Chain:
+def get_chain_by_pdfs() -> Chain | None:
+    if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
+        st.error("❌ Please set your OpenAI API key in the sidebar.")
+        return
+    
     # get documents
     documents = get_docs_from_pdfs()
     # split documents into chunks
@@ -71,8 +75,12 @@ def get_chain_by_pdfs()->Chain:
     llm = get_llm()
     # Setup memory for contextual conversation
     msgs = StreamlitChatMessageHistory(key="langchain_messages")
-    memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=msgs, return_messages=True)
-    chain = ConversationalRetrievalChain.from_llm(retriever=retriever,llm=llm,memory=memory)
+    memory = ConversationBufferMemory(
+        memory_key="chat_history", chat_memory=msgs, return_messages=True
+    )
+    chain = ConversationalRetrievalChain.from_llm(
+        retriever=retriever, llm=llm, memory=memory
+    )
     return chain
 
 
@@ -87,16 +95,23 @@ def set_input_area():
     if len(st.session_state["pdf_docs"]) == 0:
         st.info("Upload your PDFs first.")
         return
-    if st.session_state["pdf_processed"] == False:
+    if st.session_state["pdfs_processed"] == False:
         st.info("Click on 'Process' first.")
         return
+    
     user_question = st.text_input("Start your talk here:")
-    if user_question and st.session_state["chain"] is not None and st.session_state["recently_cleared_chat_history"] is False:
+    if user_question == "":
+        return
+    if (
+        user_question
+        and st.session_state["chain"] is not None
+        and st.session_state["recently_cleared_chat_history"] is False
+    ):
         handle_userinput(user_question)
 
 
 def handle_userinput(user_question: str):
-    '''This function handles user input.'''
+    """This function handles user input."""
     response = st.session_state["chain"](user_question)
     st.write(response)
 
@@ -119,24 +134,33 @@ def set_sidebar_upload_pdf_module():
 def set_sidebar_about_project_module():
     with st.sidebar:
         st.header("About")
-        st.write("This is a demo of EduSocrates, a chatbot that helps you review what you've learned.")
-        st.write("This is a project of [Agent Kindergarten](https://github.com/Agent-Kindergarten).")
-        st.write("You can find the source code [here](https://github.com/Agent-Kindergarten/EduSocrates)")
+        st.write(
+            "This is a demo of EduSocrates, a chatbot that helps you review what you've learned."
+        )
+        st.write(
+            "This is a project of [Agent Kindergarten](https://github.com/Agent-Kindergarten)."
+        )
+        st.write(
+            "You can find the source code [here](https://github.com/Agent-Kindergarten/EduSocrates)"
+        )
 
 
 def set_sidebar_settings_module():
     with st.sidebar:
         st.header("Settings")
         st.write("You can change the following settings:")
-        st.text_input(label="- OpenAI API key",type="password",key="webpage_openai_api_key")
-        if st.button(label="Save settings",key="webpage_save_settings"):
+        st.text_input(
+            label="- OpenAI API key", type="password", key="webpage_openai_api_key"
+        )
+        if st.button(label="Save settings", key="webpage_save_settings"):
             os.environ["OPENAI_API_KEY"] = st.session_state["webpage_openai_api_key"]
             st.success("✅ Settings saved!")
+
 
 def set_sidebar_actions_module():
     with st.sidebar:
         st.header("Actions")
-        if st.button(label="Clear chat history",key="recently_cleared_chat_history"):
+        if st.button(label="Clear chat history", key="recently_cleared_chat_history"):
             st.session_state["langchain_messages"].clear()
             st.success("✅ Chat history cleared!")
 
@@ -149,8 +173,33 @@ def get_chain_when_pressing_process_button():
             if st.button(label="Process", disabled=False):
                 with st.spinner("Processing"):
                     st.session_state["chain"] = get_chain_by_pdfs()
-                st.success("✅ PDFs has been processed!")
-                st.session_state["pdf_processed"] = True
+                if st.session_state["chain"] is None:
+                    st.error("❌ Processing failed.")
+                    return
+                st.success("✅ PDFs have been processed!")
+                st.session_state["pdfs_processed"] = True
+
+
+def set_chat_display_module():
+    msgs = st.session_state["langchain_messages"]
+    print("1. ",st.session_state["langchain_messages"])
+    print("2. ",msgs)
+    print("3. ",type(msgs))
+    print("4. ",msgs.messages)
+    if len(msgs.messages) == 0:
+        msgs.add_ai_message("Hello, I'm Socrates, and how can I help you?")
+
+    avatars = {"human": "user", "ai": "assistant"}
+    for msg in msgs.messages:
+        st.chat_message(avatars[msg.type]).write(msg.content)
+
+    if user_query := st.chat_input(placeholder="Ask me anything!"):
+        st.chat_message("user").write(user_query)
+
+        # with st.chat_message("assistant"):
+        #     retrieval_handler = PrintRetrievalHandler(st.container())
+        #     stream_handler = StreamHandler(st.empty())
+        #     response = qa_chain.run(user_query, callbacks=[retrieval_handler, stream_handler])
 
 
 def main():
@@ -162,8 +211,8 @@ def main():
         st.session_state["pdf_docs"] = []
     if "chain" not in st.session_state:
         st.session_state["chain"] = None
-    if "pdf_processed" not in st.session_state:
-        st.session_state["pdf_processed"] = False
+    if "pdfs_processed" not in st.session_state:
+        st.session_state["pdfs_processed"] = False
     if "langchain_messages" not in st.session_state:
         st.session_state["langchain_messages"] = []
 
@@ -179,15 +228,17 @@ def main():
     # get chain when pressing process button
     get_chain_when_pressing_process_button()
 
-    # set user input area if pdfs have been uploaded
-    set_input_area()
+    # # set user input area if pdfs have been uploaded
+    # set_input_area()
 
     # set sidebar actions module
     set_sidebar_actions_module()
 
     # set sidebar about project module
     set_sidebar_about_project_module()
-    
+
+    # set chat display module
+    set_chat_display_module()
 
 
 if __name__ == "__main__":

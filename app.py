bf3319e44e36d7b82c0c92dabbbbd9a9d745c7de
Royce Lee - Li Ji <445081882@qq.com>
2023-10-11 15:06:55 +0800

    chat in ConversationalRetrievalChain style

--- app.py
+++ app.py
@@ -1,24 +1,67 @@
-from typing import List
-from dotenv import load_dotenv
-from deprecated import deprecated
 import os
 import tempfile
 import streamlit as st
-from streamlit.runtime.uploaded_file_manager import UploadedFile
-from langchain.docstore.document import Document
-from langchain.document_loaders.pdf import PyPDFLoader
-from langchain.schema.vectorstore import VectorStore
-from langchain.schema.retriever import BaseRetriever
-from langchain.vectorstores.chroma import Chroma
-from langchain.vectorstores.faiss import FAISS
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from typing import List
+from dotenv import load_dotenv
+from deprecated import deprecated
 from langchain.chains import ConversationChain, ConversationalRetrievalChain
 from langchain.chains.base import Chain
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.chat_models import ChatOpenAI
+from langchain.docstore.document import Document
+from langchain.document_loaders.pdf import PyPDFLoader
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
 from langchain.llms.openai import OpenAI
 from langchain.llms.base import BaseLLM
 from langchain.memory import ConversationKGMemory, ConversationBufferMemory
 from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+from streamlit.runtime.uploaded_file_manager import UploadedFile
+from langchain.schema.vectorstore import VectorStore
+from langchain.schema.retriever import BaseRetriever
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.chroma import Chroma
+from langchain.vectorstores.faiss import FAISS
+
+
+class StreamHandler(BaseCallbackHandler):
+    def __init__(
+        self, container: st.delta_generator.DeltaGenerator, initial_text: str = ""
+    ):
+        self.container = container
+        self.text = initial_text
+        self.run_id_ignore_token = None
+
+    def on_llm_start(self, serialized: dict, prompts: list, **kwargs):
+        # Workaround to prevent showing the rephrased question as output
+        if prompts[0].startswith("Human"):
+            self.run_id_ignore_token = kwargs.get("run_id")
+
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        if self.run_id_ignore_token == kwargs.get("run_id", False):
+            return
+        self.text += token
+        self.container.markdown(self.text)
+
+
+class PrintRetrievalHandler(BaseCallbackHandler):
+    def __init__(self, container):
+        self.status = container.status("**Context Retrieval**")
+
+    def on_retriever_start(self, serialized: dict, query: str, **kwargs):
+        self.status.write(f"**Question:** {query}")
+        if type(st.session_state["chain"]) is ConversationalRetrievalChain:
+            self.status.update(
+                label=f'**Context Retrieval:** {query} \n:warning: :orange[*Note that this retrieval question is generated based on the question you asked.*]'
+            )
+        else:
+            self.status.update(label=f"**Context Retrieval:** {query}")
+
+    def on_retriever_end(self, documents, **kwargs):
+        for idx, doc in enumerate(documents):
+            source = os.path.basename(doc.metadata["source"])
+            self.status.write(f"**Document {idx} from {source}**")
+            self.status.markdown(doc.page_content)
+        self.status.update(state="complete")
 
 
 def get_docs_from_pdfs() -> List[Document]:
@@ -59,7 +102,7 @@ def get_retriever(vectorstore: VectorStore) -> BaseRetriever:
 
 
 def get_llm() -> BaseLLM:
-    return OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
+    return ChatOpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"), streaming=True)
 
 
 def get_chain_by_pdfs() -> Chain | None:
@@ -80,7 +123,7 @@ def get_chain_by_pdfs() -> Chain | None:
         memory_key="chat_history", chat_memory=msgs, return_messages=True
     )
     chain = ConversationalRetrievalChain.from_llm(
-        retriever=retriever, llm=llm, memory=memory
+        retriever=retriever, llm=llm, memory=memory, verbose=True
     )
     return chain
 
@@ -91,7 +134,7 @@ def set_main_page():
     # st.write(css, unsafe_allow_html=True)
 
 
-@deprecated(reason="use st.chat_input() instead")
+@deprecated(reason="use set_chat_display_module() instead")
 def set_input_area():
     st.header(":books: Talk to Socrates what you've just learned.")
     if len(st.session_state["pdf_docs"]) == 0:
@@ -199,15 +242,19 @@ def set_chat_display_module():
 
     user_query = st.chat_input(placeholder="Ask me anything!")
     if user_query is not None:
-        st.chat_message(avatar="user").write(user_query)
+        st.chat_message(name="user").write(user_query)
         if chain is not None:
-            response = chain(user_query)
-            st.chat_message(avatar="🧙‍♂️").write(response["answer"])
+            # response = chain(user_query)
+            # st.chat_message(name="Socrates", avatar="🧙‍♂️").write(response["answer"])
 
-        # with st.chat_message("assistant"):
-        #     retrieval_handler = PrintRetrievalHandler(st.container())
-        #     stream_handler = StreamHandler(st.empty())
-        #     response = qa_chain.run(user_query, callbacks=[retrieval_handler, stream_handler])
+            with st.chat_message(name="Socrates", avatar="🧙‍♂️"):
+                retrieval_handler = PrintRetrievalHandler(st.container())
+                stream_handler = StreamHandler(st.empty())
+                print(user_query, chain.input_keys)
+                response = chain(
+                    inputs={"question": user_query},
+                    callbacks=[retrieval_handler, stream_handler],
+                )
 
 
 def main():
